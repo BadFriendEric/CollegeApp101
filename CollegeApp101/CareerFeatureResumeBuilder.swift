ccbdfc7061b0a5fa5237d16a5ae99df3640ac0c9
Eric Ota <theericota@gmail.com>
2016-08-14 15:00:09 -0700

    Makes white cards add to the bottom.  NOT QUITE BOIZ

--- CollegeApp101/CareerFeatureResumeBuilder.swift
+++ CollegeApp101/CareerFeatureResumeBuilder.swift
@@ -13,7 +13,8 @@ import UIKit
 public class CareerFeatureResumeBuilder: CareerFeature{
     
     var resumeCards: [ResumeCard] = []
-    
+    private var resumeCardSpacing = 25
+    private var resumeCardBottom = 50
     
     
     public override init() {
@@ -36,7 +37,7 @@ public class CareerFeatureResumeBuilder: CareerFeature{
     private func prepareMainButtons(){
         
         let button: FlatButton = FlatButton()
-        button.frame = CGRect(x: 50, y: 50, width: 200, height: 50)
+        button.frame = CGRect(x: width/2 + 20, y: 50, width: 75, height: 25)
         button.setTitleColor(MaterialColor.white, forState: .Normal)
         button.titleLabel?.font = UIFont(name: "Scope One", size: 10)
         button.setTitle("Add Card", forState: .Normal)
@@ -48,17 +49,45 @@ public class CareerFeatureResumeBuilder: CareerFeature{
 
     
     internal func handleAddResumeCard(){
-        addResumeCard(50, size: 200, vc: self.scrollView)
-        
+        let size = 110
+
+        addResumeCard(50, vc: self.scrollView)
+        resumeCardBottom += size+resumeCardSpacing
+
+        if(resumeCardBottom > Int(self.view.frame.height)){
+            refreshScrollView(resumeCardBottom)
+        }
     }
     
-    internal func addResumeCard(y: Int, size: Int, vc: UIScrollView){
-        let card: ResumeCard = ResumeCard(vc: self, x: 0 + 15, y: 140, width: width - 30, height: 100)
+    internal func addResumeCard(y: Int, vc: UIScrollView){
+        let card: ResumeCard = ResumeCard(vc: self, x: 0 + 15, y: 140 + 100*(resumeCards.count), width: width - 30, height: 100)
         resumeCards.append(card)
         vc.addSubview(card)
         
         
+
+        
     }
+    
+    internal func refreshResumeCards(){
+        //print("refresh")
+        var currentCardPos = 70 //was nav bar height
+        for card in resumeCards{
+            if(Int(card.y) > currentCardPos){
+                UIView.animateWithDuration(0.3, animations: {
+                    card.y = CGFloat(currentCardPos)
+                })
+            }
+            currentCardPos = Int(card.y) + Int(card.height) + resumeCardSpacing
+        }
+        if(resumeCards.isEmpty){
+            resumeCardBottom = 70 //was nav bar height
+        }else{
+            resumeCardBottom = Int((resumeCards.last?.y)!) + Int((resumeCards.last?.height)!) + resumeCardSpacing
+        }
+        self.refreshScrollView(self.resumeCardBottom)
+    }
+
     
     public override func getFeatures() -> [UIView] {
         return features
