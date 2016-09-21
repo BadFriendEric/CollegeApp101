46536b6f5657410a561ce774c664054af30fd7e5
Eric Ota <theericota@gmail.com>
2016-09-21 14:32:06 -0700

    Safe 2 pull I think.  Newest resume builder

--- CollegeApp101/CareerFeatureResumeBuilder.swift
+++ CollegeApp101/CareerFeatureResumeBuilder.swift
@@ -10,11 +10,11 @@ import Foundation
 import Material
 import UIKit
 
-open class CareerFeatureResumeBuilder: CareerFeature, UITextViewDelegate {
+public class CareerFeatureResumeBuilder: CareerFeature, UITextViewDelegate {
     
     var resumeCards: [ResumeCard] = []
-    fileprivate var resumeCardSpacing = 25
-    fileprivate var resumeCardBottom = 50
+    private var resumeCardSpacing = 25
+    private var resumeCardBottom = 50
     let iconSize = 50
     var words: String = "vibes"
     var timeWords: String = "chill"
@@ -24,7 +24,12 @@ open class CareerFeatureResumeBuilder: CareerFeature, UITextViewDelegate {
     let timeText: UITextView = UITextView()
     var textBoxSize = 50
     let button: FlatButton = FlatButton()
-
+    let buttonWidth: Int = 75
+    let buildBtn: FlatButton = FlatButton()
+    let buildBtnWidth: Int = 100
+    let adviceBtn: FlatButton = FlatButton()
+    var coachingLineMid: UIView! = UIView()
+    
     public override init() {
         super.init()
     }
@@ -33,34 +38,66 @@ open class CareerFeatureResumeBuilder: CareerFeature, UITextViewDelegate {
     public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
     
-    open override func setFrame(_ x: Int, y: Int, width: Int, height: Int) {  //open Resume Builder
+    
+    public override func setFrame(_ x: Int, y: Int, width: Int, height: Int) {  //open Resume Builder
         super.setFrame(x, y: y, width: width, height: height)
         setFeatureTitle("Resume Builder")
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(backgroundTapped))
         tap.delegate = self
         scrollView.addGestureRecognizer(tap)
         prepareMainButtons()
+        prepareBuildButton()
+        prepareAdviceButton()
         prepareTextField()
+        prepareRandomUI()
         prepareTimeText()
-        refreshScrollView(1000) //use when pushing stuff off the screen
+        refreshScrollView(600) //use when pushing stuff off the screen
     }
     
-  
     
-    fileprivate func prepareMainButtons(){
-        button.frame = CGRect(x: width/2 + width/4 - 25, y: 100, width: 75, height: 25)
-        button.setTitleColor(Color.white, for: UIControlState())
+    
+    private func prepareMainButtons(){
+        button.frame = CGRect(x: width/2 + width/4 - 25, y: 100, width: buttonWidth, height: 25)
+        button.setTitleColor(Color.white, for: .normal)
         button.titleLabel?.font = UIFont(name: "Scope One", size: 10)
-        button.setTitle("Add Card", for: UIControlState())
+        button.setTitle("Add Card", for: .normal)
         button.backgroundColor = Color.grey.base
+        button.pulseColor = Color.red.darken4
         button.addTarget(self, action: #selector(handleAddResumeCard), for: .touchUpInside)
         features.append(button)
         
     }
     
-    fileprivate func prepareTextField(){
+    private func prepareBuildButton(){
+        buildBtn.frame = CGRect(x: 40, y: 140, width: buildBtnWidth, height: 30)
+        buildBtn.setTitleColor(Color.white, for: .normal)
+        buildBtn.titleLabel?.font = UIFont(name: "Scope One", size: 10)
+        buildBtn.setTitle("Build Resume", for: .normal)
+        buildBtn.backgroundColor = Color.orange.darken2
+        buildBtn.pulseColor = Color.red.darken4
+        buildBtn.addTarget(self, action: #selector(handleBuildBtn), for: .touchUpInside)
+        features.append(buildBtn)
+    }
+    
+    private func prepareRandomUI(){
+        //nothuing here rn lol
+    }
+    
+    private func prepareAdviceButton(){
+        adviceBtn.frame = CGRect(x: width - 210, y: 140, width: 170, height: 30)
+        adviceBtn.setTitleColor(Color.white, for: .normal)
+        adviceBtn.titleLabel?.font = UIFont(name: "Scope One", size: 10)
+        adviceBtn.setTitle("How to improve your resume", for: .normal)
+        adviceBtn.backgroundColor = Color.orange.darken2
+        adviceBtn.pulseColor = Color.red.darken4
+        adviceBtn.addTarget(self, action: #selector(handleHowToBtn), for: .touchUpInside)
+        features.append(adviceBtn)
+    }
+    
+    
+    
+    private func prepareTextField(){
         textBox.frame = CGRect(x: 20, y: 10, width: width - 40, height: 80)
         textBox.delegate = self
         textBox.backgroundColor = Color.white
@@ -68,11 +105,12 @@ open class CareerFeatureResumeBuilder: CareerFeature, UITextViewDelegate {
         textBox.textColor = Color.grey.lighten1
         textBox.text! = placeHolder
         textBox.layer.cornerRadius = 10
+        //textBox.clearsOnInsertion = true
         features.append(textBox)
         
     }
     
-    fileprivate func prepareTimeText(){
+    private func prepareTimeText(){
         timeText.frame = CGRect(x: 20, y: 100, width: width - 150, height: 30)
         timeText.delegate = self
         timeText.backgroundColor = Color.white
@@ -80,13 +118,20 @@ open class CareerFeatureResumeBuilder: CareerFeature, UITextViewDelegate {
         timeText.textColor = Color.grey.lighten1
         timeText.text! = placeHolderTime
         timeText.layer.cornerRadius = 10
+        //timeText.clearsOnInsertion = true
         features.append(timeText)
-
+        
     }
     
     internal func handleAddResumeCard(){
         let size = 110
-        if button.backgroundColor == Color.amber.base{
+        
+        if textBox.hasText && timeText.hasText && button.backgroundColor == Color.grey.base{
+            button.backgroundColor = Color.amber.base
+            textBox.resignFirstResponder()
+            timeText.resignFirstResponder()
+        }
+        else if button.backgroundColor == Color.amber.base{
             words = textBox.text!
             timeWords = timeText.text!
             textBox.textColor = Color.grey.lighten1
@@ -95,7 +140,7 @@ open class CareerFeatureResumeBuilder: CareerFeature, UITextViewDelegate {
             timeText.text = placeHolderTime
             textBox.resignFirstResponder()
             timeText.resignFirstResponder()
-            addResumeCard(50, vc: self.scrollView, text: words)
+            addResumeCard(y: 50, vc: self.scrollView, text: words)
             resumeCardBottom += size+resumeCardSpacing
         }
         else{
@@ -106,15 +151,24 @@ open class CareerFeatureResumeBuilder: CareerFeature, UITextViewDelegate {
             textBox.resignFirstResponder()
             timeText.resignFirstResponder()
             //ADD POPUP HERE
-
+            
         }
-
+        
         if(resumeCardBottom > Int(self.view.frame.height)){
             refreshScrollView(resumeCardBottom)
         }
     }
     
-    internal func addResumeCard(_ y: Int, vc: UIScrollView, text: String){
+    internal func handleBuildBtn(){
+        //build resume items into resume
+    }
+    
+    internal func handleHowToBtn(){
+        //build resume items into resume
+    }
+    
+    
+    internal func addResumeCard(y: Int, vc: UIScrollView, text: String){
         let card: ResumeCard = ResumeCard(vc: self, x: 25, y: 180 + 110*(resumeCards.count), width: width - 50, height: 100, text: text, text2: timeWords)
         button.backgroundColor = Color.grey.base
         resumeCards.append(card)
@@ -124,7 +178,7 @@ open class CareerFeatureResumeBuilder: CareerFeature, UITextViewDelegate {
     
     internal func refreshResumeCards(){
         //print("refresh")
-        var currentCardPos = 70 //was nav bar height
+        var currentCardPos = 70 //was nav bar heigth
         for card in resumeCards{
             if(Int(card.y) > currentCardPos){
                 UIView.animate(withDuration: 0.3, animations: {
@@ -140,23 +194,22 @@ open class CareerFeatureResumeBuilder: CareerFeature, UITextViewDelegate {
         }
         self.refreshScrollView(self.resumeCardBottom)
     }
-
     
-    open override func getFeatures() -> [UIView] {
+    
+    public override func getFeatures() -> [UIView] {
         return features
     }
     
-    internal func deleteResumeCard(_ card: ResumeCard){
-        /* 
+    internal func deleteResumeCard(card: ResumeCard){
         card.removeFromSuperview()
-        resumeCards.removeAtIndex(resumeCards.indexOf(card)!)
+        resumeCards.remove(at: resumeCards.index(of: card)!)
         refreshResumeCards()
-         */
+        
     }
     
     
     func backgroundTapped(){
-        if (textBox.text != "" && timeText.text != "" && textBox.text != placeHolder && timeText.text != placeHolderTime){
+        if (textBox.hasText && textBox.text != placeHolder && timeText.hasText && timeText.text != placeHolderTime){
             button.backgroundColor = Color.amber.base
         }
         else{
@@ -166,26 +219,31 @@ open class CareerFeatureResumeBuilder: CareerFeature, UITextViewDelegate {
         timeText.resignFirstResponder()
     }
     
-    open func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+    public func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
         if textView.textColor == Color.grey.lighten1{
             textView.text! = ""
             textView.textColor = Color.black
         }
+        
+        if textBox.hasText && timeText.hasText{
+            button.backgroundColor = Color.amber.base
+        }
+        
         return true
     }
     
-    open func textViewDidEndEditing(_ textView: UITextView) {
-        if textView.text == ""{
+    public func textViewDidEndEditing(_ textView: UITextView) {
+        if !textView.hasText{
             textView.textColor = Color.grey.lighten1
             textBox.text = placeHolder
             timeText.text = placeHolderTime
         }
         
-        if (textBox.text != "" && timeText.text != "" && textBox.text != placeHolder && timeText.text != placeHolderTime){
+        if (!textBox.hasText && !timeText.hasText && textBox.text != placeHolder && timeText.text != placeHolderTime){
             button.backgroundColor = Color.amber.base
         }
     }
     
     
-
+    
 }
--- CollegeApp101/ResumeCard.swift
+++ CollegeApp101/ResumeCard.swift
@@ -1,6 +1,6 @@
 //
 //  ResumeCard.swift
-//  
+//
 //
 //  Created by Eric Ota on 8/13/16.
 //
@@ -13,33 +13,42 @@ import Material
 class ResumeCard: Card{
     
     var view: CareerFeatureResumeBuilder = CareerFeatureResumeBuilder()
-
+    
     var trash: IconButton = IconButton()
+    var editBtn: IconButton = IconButton()
     var trashClicked = false
     let trashImg: UIImage? = Icon.cm.close
-    
+    let editImg: UIImage? = Icon.cm.pen
+    let editColor: UIColor = Color.red.darken4
     var mysubviews = [UIView]()
     var text: String = "yes"
     var textTime: String = "pls"
     var icon: UIImage = UIImage()
-    let iconSize = 30
-
+    let iconSize: Int = 30
+    let buttonArea = 55
+    var editOn: Bool = false
     
+    var boxTime: UITextField = UITextField()
+    var box: UITextField = UITextField()
+    
+    //BUG TO FIX: WHEN U PRESS
     
     internal init(vc: CareerFeatureResumeBuilder, x: Int, y: Int, width: Int, height: Int, text: String, text2: String){
         super.init(frame: CGRect(x: x, y: y, width: width, height: height))
         self.view = vc
         divider = false
         setTrashButton()
-        setTextBox(text, text2: text2)
+        setTextBox(text: text, text2: text2)
+        setEdit()
         setIcon()
+        setTimeIcon()
         reloadView()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func reloadView() {
         for v in subviews {
             v.removeFromSuperview()
@@ -48,22 +57,22 @@ class ResumeCard: Card{
             self.addSubview(v)
         }
     }
-
+    
     
     
     internal func setTrashButton(){
-        trash = IconButton(frame: CGRect(x: Int(self.frame.width)-45, y: Int(self.frame.height - 10), width: Int(self.frame.width) - iconSize - 50, height: Int(self.frame.height)-35))
+        trash = IconButton(frame: CGRect(x: Int(self.frame.width)-45, y: 0, width: 50, height: 35))
         trash.pulseColor = Color.red.base
         trash.tintColor = Color.black
-        trash.setImage(trashImg, for: UIControlState())
+        trash.setImage(trashImg, for: .normal)
         trash.setImage(trashImg, for: .highlighted)
-        trash.addTarget(self, action: #selector(handleTrashButton), for: .touchUpInside)  //cosider changing to touch down
+        trash.addTarget(self, action: #selector(handleTrashButton), for: .touchDown)  //cosider changing to touch down
         
         mysubviews.append(trash)
     }
     
     internal func resetTrashButton(){
-        //let trashImg: UIImage? = Icon.cm.close
+        //let trashImg: UIImage? = MaterialIcon.cm.close
         trash.tintColor = Color.black
         //trash.setImage(trashImg, forState: .Normal)
         //trash.setImage(trashImg, forState: .Highlighted)
@@ -78,9 +87,9 @@ class ResumeCard: Card{
     
     internal func handleTrashButton(){
         if(trashClicked){
-            view.deleteResumeCard(self)
+            view.deleteResumeCard(card: self)
         }else{
-            //let trashImg: UIImage? = Icon.check
+            //let trashImg: UIImage? = MaterialIcon.check
             UIView.animate(withDuration: 0.2, animations: {
                 self.trash.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI/2))
                 }, completion: { finished in
@@ -92,75 +101,126 @@ class ResumeCard: Card{
         
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesEnded(touches, with: event)
+    func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        super.touchesEnded(touches as! Set<UITouch>, with: event)
         if(trashClicked){
             resetTrashButton()
         }
         
     }
     
-    internal func setTextBox(_ text: String, text2: String){
-        let box = UITextField(frame: CGRect(x: iconSize + 20, y: 0, width: Int(self.frame.width) - iconSize - 50, height: Int(self.frame.height)/2))
-        //box.backgroundColor = Color.pink.base
+    internal func setTextBox(text: String, text2: String){
+        box = UITextField(frame: CGRect(x: iconSize + 15, y: 2, width: Int(self.frame.width) - iconSize - buttonArea, height: Int(self.frame.height)/2-5))
+        box.backgroundColor = Color.white
         box.textColor = Color.black
         box.font = RobotoFont.light(with: 16)
         box.text = text
         box.adjustsFontSizeToFitWidth = true
+        box.minimumFontSize = 10
+        box.isUserInteractionEnabled = false
+        box.textAlignment = .left
+        box.borderStyle = .roundedRect
         mysubviews.append(box)
         
-        let boxTime = UITextField(frame: CGRect(x: iconSize + 20, y: Int(self.frame.height)/2, width: Int(self.frame.width) - iconSize - 50, height: Int(self.frame.height)/2))
-        //boxTime.backgroundColor = Color.green.base
+        boxTime = UITextField(frame: CGRect(x: iconSize + 15, y: Int(self.frame.height)/2+2, width: Int(self.frame.width) - iconSize - buttonArea, height: Int(self.frame.height)/2-5))
+        boxTime.backgroundColor = Color.white
         boxTime.textColor = Color.black
         boxTime.font = RobotoFont.light(with: 16)
         boxTime.text = text2
+        boxTime.textAlignment = .left
+        boxTime.borderStyle = .roundedRect
         boxTime.adjustsFontSizeToFitWidth = true
+        boxTime.minimumFontSize = 10
+        boxTime.isUserInteractionEnabled = false
         mysubviews.append(boxTime)
-
+        
     }
-
+    
     internal func setIcon(){
         self.icon = Icon.menu!
-        //_ = self.icon.tintWithColor(color: MainPanels.Constants.Color3)
         let iconView = UIImageView(image: self.icon)
-        iconView.frame = CGRect(x: 5, y: 5, width: iconSize, height: iconSize)
+        iconView.tintColor = editColor
+        iconView.frame = CGRect(x: 5, y: 10, width: iconSize, height: iconSize)
+        mysubviews.append(iconView)
+        
+    }
+    
+    internal func setTimeIcon(){
+        self.icon = Icon.bell!
+        let iconView = UIImageView(image: self.icon)
+        iconView.tintColor = editColor
+        iconView.frame = CGRect(x: 5, y: Int(height) - 10 - iconSize, width: iconSize, height: iconSize)
         mysubviews.append(iconView)
         
     }
     
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    internal func setEdit(){
+        editBtn = IconButton(frame: CGRect(x: Int(width) - 45, y: Int(height) - 10 - iconSize, width: 50, height: 35))
+        editBtn.pulseColor = Color.amber.darken4
+        editBtn.tintColor = editColor
+        editBtn.setImage(editImg, for: .normal)
+        editBtn.setImage(editImg, for: .highlighted)
+        editBtn.addTarget(self, action: #selector(handleEditButton), for: .touchUpInside)
+        
+        mysubviews.append(editBtn)
+        
+    }
+    
+    internal func handleEditButton(){
+        if !editOn {
+            editOn = true
+            box.isUserInteractionEnabled = true
+            boxTime.isUserInteractionEnabled = true
+            box.backgroundColor = Color.grey.lighten2
+            boxTime.backgroundColor = Color.grey.lighten2
+        }
+        else{
+            editOn = false
+            box.isUserInteractionEnabled = false
+            boxTime.isUserInteractionEnabled = false
+            box.backgroundColor = Color.white
+            boxTime.backgroundColor = Color.white
+        }
+        
+        
+    }
+    
+    
+    
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
     
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
         return true
     }
     
-    func textFieldDidBeginEditing(_ textField: UITextField) {
+    func textFieldDidBeginEditing(textField: UITextField) {
     }
     
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+    func textFieldShouldEndEditing(textField: UITextField) -> Bool {
         return true
     }
     
-    func textFieldDidEndEditing(_ textField: UITextField) {
+    func textFieldDidEndEditing(textField: UITextField) {
         (textField as? ErrorTextField)?.revealError = false
     }
     
-    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+    func textFieldShouldClear(textField: UITextField) -> Bool {
         (textField as? ErrorTextField)?.revealError = false
         return true
     }
     
-    func textField(_ textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
         (textField as? ErrorTextField)?.revealError = false
         return true
     }
-
-
     
-        
+    
+    
+    
     
 }
