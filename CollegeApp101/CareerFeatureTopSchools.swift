7a7265fa2b9b1a1a468d5d87120c901875544bf2
Sam Hollenbach <shollenb@macalester.edu>
2016-09-21 15:07:57 -0500

    List is now draggable, and has changeable color indicator

--- CollegeApp101/CareerFeatureTopSchools.swift
+++ CollegeApp101/CareerFeatureTopSchools.swift
@@ -14,9 +14,12 @@ open class CareerFeatureTopSchools : CareerFeature {
  
     //0 for List View, 1 for Search View
     var currentView : Int = 0
+    var schools : [TopSchoolsListItem]! = [TopSchoolsListItem]()
+    var bottomY = 10
     
     public override init() {
         super.init()
+        prepareList()
     }
     
     
@@ -24,11 +27,57 @@ open class CareerFeatureTopSchools : CareerFeature {
         fatalError("init(coder:) has not been implemented")
     }
     
+    open func prepareList(){
+        
+        for i in 0..<10 {
+            let t = TopSchoolsListItem(schoolName: "MySchool University #"+String(i+1), feature: self)
+            t.setListPosition(i: i + 1)
+            schools.append(t)
+        }
+        
+        
+        
+        refreshList()
+        
+        
+    }
+    
+    
+    open func swapItems(a : Int, b : Int){
+        
+        let t = schools[a-1]
+        schools[a-1] = schools[b-1]
+        schools[b-1] = t
+        refreshList()
+    }
+    
+    open func refreshList(){
+        
+        bottomY = 10
+        let tl = UIView(frame: CGRect(x: 0, y: bottomY-2, width: Int(MainSwipeController.Constants.ScreenWidth), height: 2))
+        tl.backgroundColor = Color.white
+        features.append(tl)
+        var i = 1
+        for s in schools {
+            s.setYPosition(bottomY)
+            s.itemNumber = i
+            i += 1
+            bottomY = Int(s.frame.maxY)
+            //print(bottomY)
+            features.append(s)
+            s.refreshTitle()
+            
+        }
+        
+        refreshScrollView(bottomY)
+        //updateFrame()
+    }
+    
     
     open override func setFrame(_ x: Int, y: Int, width: Int, height: Int) {
         super.setFrame(x, y: y, width: width, height: height)
         setFeatureTitle("Top Schools")
-        refreshScrollView(height)
+        //refreshScrollView(height)
     }
     
         open override func updateFrame(){
--- CollegeApp101/TopSchoolsListItem.swift
+++ CollegeApp101/TopSchoolsListItem.swift
@@ -15,27 +15,211 @@ class TopSchoolsListItem : UIView {
     
     var schoolName : String = ""
     var feature : CareerFeatureTopSchools? = nil
-    var itemNumber = 0
+    var itemNumber = 1
     let h = CGFloat(60)
+    let dot = CAShapeLayer()
+    let n = UILabel(frame: CGRect(x: 30, y: 12, width: 45, height: 35))
+    let l = UILabel(frame: CGRect(x: 67, y: 15, width: MainSwipeController.Constants.ScreenWidth-150, height: 30))
+    let d = UIView(frame: CGRect(x: MainSwipeController.Constants.ScreenWidth-50, y: 0.0, width: 50, height: 30))
+    let tl = UIView(frame: CGRect(x: 0, y: 0, width: MainSwipeController.Constants.ScreenWidth, height: 2))
+    let bl = UIView(frame: CGRect(x: 0, y: 58, width: MainSwipeController.Constants.ScreenWidth, height: 2))
+    let ll = UIView(frame: CGRect(x: 0, y: 0, width: 2, height: 60))
+    let rl = UIView(frame: CGRect(x: MainSwipeController.Constants.ScreenWidth-2, y: 0, width: 2, height: 60))
+    var pageControllerData : UIPageViewControllerDataSource? = nil
+    var canMove = true
+    var moving = false
     
     internal init(schoolName : String, feature : CareerFeatureTopSchools){
         super.init(frame: CGRect(x: 0, y: 0, width: Int(MainSwipeController.Constants.ScreenWidth), height: Int(h)))
         self.schoolName = schoolName
         self.feature = feature
+        prepareLines()
+        prepareNumberLabel()
+        prepareTitleLabel()
+        prepareColorLabel()
+        //prepareDragIcon()
+        
     }
     
     internal func setYPosition(_ y : Int){
         frame = CGRect(x: 0, y: y, width: Int(MainSwipeController.Constants.ScreenWidth), height: Int(h))
     }
     
-    internal func prepareTitleLabel(){
-        let l = UILabel(frame: CGRect(x: 0, y: 15, width: frame.width, height: 30))
-        l.text = schoolName
-        l.textColor = Color.white
-        l.font = RobotoFont.bold(with: 18)
-        self.addSubview(l)
+    internal func prepareLines(){
+        
+        tl.backgroundColor = Color.white
+        bl.backgroundColor = Color.white
+        self.addSubview(tl)
+        self.addSubview(bl)
     }
     
+    internal func prepareColorLabel(){
+        //let size = self.frame.height
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: self.frame.width-35,y: self.frame.height/2), radius: CGFloat(15), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
+        
+        
+        dot.path = circlePath.cgPath
+        
+        //dot.fillColor = Color.red.base.cgColor
+        dot.lineWidth = 20
+        self.layer.addSublayer(dot)
+    }
+    
+    internal func prepareNumberLabel(){
+        n.text = String(itemNumber) + "."
+        n.textColor = Color.white
+        n.font = UIFont(name: "Oswald", size: 22)
+        self.addSubview(n)
+    }
+    
+    internal func prepareTitleLabel(){
+        
+        l.text = schoolName
+        l.textColor = Color.white
+        l.font = UIFont(name: "Vesper Libre", size: 30)
+        l.adjustsFontSizeToFitWidth = true
+        self.addSubview(l)
+        
+        
+    }
+    
+    internal func prepareDragIcon(){
+        let dragImage: UIImage? = UIImage(named: "MenuIcon")
+        //let imageSize = CGSize(width: 30, height: 30)
+        //_ = MainPanels.ResizeImage(dragImage!, targetSize: imageSize)
+        let cr = CGRect(x: MainSwipeController.Constants.ScreenWidth-45, y: 15, width: 30, height: 30)
+        dragImage?.draw(in: cr)
+        let dragImageView = UIImageView(image: dragImage)
+        dragImageView.frame = cr
+        dragImageView.image = dragImageView.image!.withRenderingMode(UIImageRenderingMode.alwaysTemplate)
+        dragImageView.tintColor = Color.white
+        self.addSubview(dragImageView)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch : UITouch = touches.first!
+        let point = touch.preciseLocation(in: self)
+        
+        if(point.x > self.frame.width - 50){
+            canMove = false
+        }else{
+            canMove = true
+            
+        }
+        //print(String(describing: point.x) + ", " + String(describing: point.y) + ", " + String(itemNumber))
+    }
+    
+    
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch : UITouch = touches.first!
+        let point = touch.preciseLocation(in: self)
+        let moveColor = Color.blue.darken3
+        
+        
+        if(!canMove){
+            return
+        }
+        if(canMove && !moving){
+            moving = true
+            self.feature?.scrollView.isScrollEnabled = false
+            tl.backgroundColor = moveColor
+            bl.backgroundColor = moveColor
+            ll.backgroundColor = moveColor
+            rl.backgroundColor = moveColor
+            self.addSubview(ll)
+            self.addSubview(rl)
+            l.font = UIFont(name: "VesperLibre-Heavy", size: 30)
+            self.backgroundColor = Color.black.withAlphaComponent(0.2)
+            
+
+            //Used to disable MainPanel paging swipe
+            //pageControllerData = self.feature?.myVC.pageViewController.dataSource
+            //self.feature?.myVC.pageViewController.dataSource = nil
+        }
+        
+        
+        if(true){
+            if(point.y < -20){
+                if(self.itemNumber > 1){
+                    feature?.swapItems(a: self.itemNumber, b: self.itemNumber-1)
+                }
+            }
+            if(point.y > self.frame.height+20){
+                if(self.itemNumber < (feature?.schools.count)!-1){
+                    feature?.swapItems(a: self.itemNumber, b: self.itemNumber+1)
+                }
+            }
+        }
+        
+    }
+    
+    
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch : UITouch = touches.first!
+        let point = touch.preciseLocation(in: self)
+        if(point.x > self.frame.width-50){
+            if(dot.fillColor == Color.red.base.cgColor){
+                dot.fillColor = Color.green.lighten1.cgColor
+            }else if(dot.fillColor == Color.blue.darken1.cgColor){
+                dot.fillColor = Color.red.base.cgColor
+            }else if(dot.fillColor == Color.green.lighten1.cgColor){
+                dot.fillColor = Color.blue.darken1.cgColor
+            }
+            return
+        }
+        
+        if(moving){
+            
+            dropItem()
+        }else{
+            self.backgroundColor = Color.white.withAlphaComponent(0.6)
+            UIView.animate(withDuration: 0.8, animations: {
+                self.backgroundColor = Color.clear
+            })
+
+        }
+        
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        dropItem()
+    }
+    
+    internal func dropItem(){
+        canMove = false
+        moving = false
+        self.feature?.scrollView.isScrollEnabled = true
+        tl.backgroundColor = Color.white
+        bl.backgroundColor = Color.white
+        ll.removeFromSuperview()
+        rl.removeFromSuperview()
+        l.font = UIFont(name: "Vesper Libre", size: 30)
+        self.backgroundColor = Color.clear
+        //UIView.animate(withDuration: 0.5, animations: {})
+
+        //self.feature?.myVC.pageViewController.dataSource = pageControllerData
+    }
+    
+    internal func refreshTitle(){
+        n.text = String(itemNumber) + "."
+        l.text = schoolName
+    }
+    
+    internal func setListPosition(i : Int){
+        self.itemNumber = i
+        if(itemNumber <= 2){
+            dot.fillColor = Color.red.base.cgColor
+        }else if(itemNumber <= 7){
+            dot.fillColor = Color.blue.darken1.cgColor
+        }else if(itemNumber <= 10){
+            dot.fillColor = Color.green.lighten1.cgColor
+        }
+        refreshTitle()
+    }
+    
+    //internal func
     
     
     
