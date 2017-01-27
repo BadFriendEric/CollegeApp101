30d077ec2b17b55f2405c76cf30aee0463329a3f
Eric Ota <theericota@gmail.com>
2017-01-26 22:00:50 -0800

    git add Pods

--- Pods/Target Support Files/Pods-CollegeApp101Tests/Pods-CollegeApp101Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CollegeApp101Tests : NSObject
-@end
-@implementation PodsDummy_Pods_CollegeApp101Tests
-@end
--- Pods/Target Support Files/Pods-CollegeApp101UITests/Pods-CollegeApp101UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CollegeApp101UITests : NSObject
-@end
-@implementation PodsDummy_Pods_CollegeApp101UITests
-@end
